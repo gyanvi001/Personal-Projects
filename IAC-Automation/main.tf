bc728c80abb31c936f25d36fb4f57acc84ba000e
gyanvi001 <gyanvipandey01@gmail.com>
2025-08-17 21:29:40 +0530

    Modified

--- IAC-Automation/main.tf
+++ IAC-Automation/main.tf
@@ -1,22 +1,59 @@
 provider "aws" {
-  region = "us-west-2"
+  region  = "us-west-2"
 }
 
 #Get the default vpc details
 data "aws_vpc" "default" {
-  default = true
+  default  = true
 }
 
 #Get default subnets from the vpc
 data "aws_subnet" "default" {
     filter {
         # only return subnets that belong to a specific VPC ID
-        name = "vpc_id"
-        values = [data.aws_vpc.default.id]
+        name    = "vpc_id"
+        values  = [data.aws_vpc.default.id]
     }
 }
 
 
+#Create a security group for the web server
+resource "aws_security_group" "Web-Sg" {
+    name         = "web-sg"
+    description  = "Allow SSH, HTTP , HTTPS"
+    vpc_id       = data.aws_vpc.default.id
+
+    ingress {
+        description = "SSH"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = [0.0.0.0/0]
+    }
+
+    ingress {
+        description = "HTTP"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = [0.0.0.0/0]
+    }
+
+    ingress {
+        description = "HTTPS"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = [0.0.0.0/0]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = [0.0.0.0/0]
+    }
+}
 
 
 
