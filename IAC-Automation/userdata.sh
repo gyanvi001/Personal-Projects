3c8979ebba1220ec5f92a2cba90621e7d41f83e3
gyanvi001 <gyanvipandey01@gmail.com>
2025-08-19 21:26:43 +0530

    Modified

--- IAC-Automation/userdata.sh
+++ IAC-Automation/userdata.sh
@@ -61,3 +61,5 @@ rm -rf aws awscliv2.zip
 # Install EKSCTL
 # ---------------------------------
 
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
