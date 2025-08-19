f207f06b93100b61177d4780b82669b5e73e2e0e
gyanvi001 <gyanvipandey01@gmail.com>
2025-08-19 21:14:31 +0530

    Modified

--- IAC-Automation/userdata.sh
+++ IAC-Automation/userdata.sh
@@ -55,3 +55,9 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip -q awscliv2.zip
 sudo ./aws/install
 rm -rf aws awscliv2.zip
+
+
+# ---------------------------------
+# Install EKSCTL
+# ---------------------------------
+
